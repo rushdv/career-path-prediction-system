ea092d893d0bcf4da61885ff1966e2ed8dae6bbb
sajibsaha388 <sajibsaha388@gmail.com>
2026-08-02 22:17:28 +0600

    Add prediction header file with career path and prediction record structures

---
+++ include/prediction.h
@@ -0,0 +1,25 @@
+#ifndef PREDICTION_H
+#define PREDICTION_H
+
+#include "assessment.h"
+
+#define NUM_CAREERS 6
+
+typedef struct {
+    char  name[50];
+    float weights[6];
+    float minRequired[6];
+} CareerPath;
+
+typedef struct {
+    int   studentRef;
+    char  timestamp[30];
+    char  topCareer[50];
+    float score;
+} PredictionRecord;
+
+void  rankCareers(SkillProfile *sp);
+float calculateScore(SkillProfile *sp, CareerPath *cp);
+void  getCareers(CareerPath out[], int *count);
+
+#endif
